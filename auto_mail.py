73757c9dbb2b4874fbbaf55685ed74a9f0a76604
Connor <303604153@qq.com>
2025-01-03 20:25:05 +0800

    Initial commit

---
+++ auto_mail.py
@@ -0,0 +1,50 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import json
+import os
+
+
+class auto_mail(object):
+    def __init__(self, config_path):
+        with open(config_path, "r", encoding="utf8") as fp:
+            config = json.load(fp)
+        self.sender = config["mail_config"]["sender"]
+        self.receiver = config["mail_config"]["recipient"]
+        self.password = config["mail_config"]["password"]
+
+    def send_email_msg(self, subject, msg):
+        # 邮件发送者
+        sender = self.sender
+        # 邮件接收者
+        receiver = self.receiver
+        # 邮件主题
+        subject = subject
+        # 邮件内容
+        body = msg
+
+        # 创建一个 multipart message
+        message = MIMEMultipart()
+        message["From"] = sender
+        message["To"] = receiver
+        message["Subject"] = subject
+        # 添加邮件内容
+        message.attach(MIMEText(body, "html"))
+
+        # 163邮箱的授权码信息
+        username = sender
+        password = self.password
+
+        # 通过163邮箱的SMTP服务器发送邮件
+        with smtplib.SMTP("smtp.163.com", 25) as server:
+            server.starttls()
+            server.login(username, password)
+            server.sendmail(sender, receiver, message.as_string())
+
+
+if __name__ == '__main__':
+    # current_file_path = os.path.abspath(__file__)
+    # config_path = os.path.join(os.path.dirname(current_file_path), "config.json")
+    # am = auto_mail(config_path)
+    # am.send_email_msg("测试邮件", "这是一封测试邮件2")
+    pass
---
+++ main.py
@@ -0,0 +1,103 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import re
+import json
+from loguru import logger
+import pandas as pd
+from auto_mail import auto_mail
+import os
+import schedule
+
+
+BANK_DIC = {
+    "icbc": "中国工商银行"
+    ,"boc": "中国银行"
+    ,"abchina": "中国农业银行"
+    ,"bankcomm": "交通银行"
+    ,"ccb": "中国建设银行"
+    ,"cmbchina": "招商银行"
+    ,"cebbank": "中国光大银行"
+    ,"spdb": "上海浦东发展银行"
+    ,"cib": "兴业银行"
+    ,"ecitic": "中信银行"
+}
+
+def extract_and_parse_json(text):
+    # 使用正则表达式提取getAllBankForex函数中的JSON部分
+    match = re.search(r'getAllBankForex\((.*?)\)', text)
+    if match:
+        json_str = match.group(1)
+        try:
+            # 解析JSON字符串
+            data = json.loads(json_str)
+            # 获取result对应的JSON
+            result = data.get('result', {})
+            return result
+        except json.JSONDecodeError as e:
+            logger.error(f'解析JSON时出错: {e}')
+    else:
+        logger.error('未找到getAllBankForex函数及其中的JSON数据')
+
+
+def crawl_webpage(url):
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+        # 发送HTTP请求获取网页内容
+        response = requests.get(url, headers=headers)
+        # 检查请求是否成功
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            return soup
+        else:
+            logger.error(f"请求失败，状态码：{response.status_code}")
+    except requests.RequestException as e:
+        logger.error(f"请求发生异常：{e}")
+
+
+@logger.catch
+def parse_usd(soup, formatted_time):
+    result_json = extract_and_parse_json(soup.prettify())
+    usd = pd.json_normalize(result_json["data"]["bank"]["USD"])
+    usd["bank_chi"] = usd["bank"].map(BANK_DIC)
+    # 银行现汇卖出越低越好，现汇买入越高越好。
+    xh_sell_min = usd["xh_sell_price"].min()
+    xh_buy_max = usd["xh_buy_price"].max()
+    best_xh_sell = usd[usd["xh_sell_price"] == xh_sell_min]
+    best_xh_buy = usd[usd["xh_buy_price"] == xh_buy_max]
+    # 避免重复值
+    best_xh_sell_bank = best_xh_sell.iloc[:,-1].to_list()
+    best_xh_buy_bank = best_xh_buy.iloc[:,-1].to_list()
+    out = f"截至{formatted_time},美元主要银行最佳现汇买入银行为{best_xh_buy_bank}，值为{xh_buy_max}；最佳现汇卖出银行为{best_xh_sell_bank}，值为{xh_sell_min}。"
+    return out
+
+
+def send_msg(obj, msg):
+    current_file_path = os.path.abspath(__file__)
+    config_path = os.path.join(os.path.dirname(current_file_path), "config.json")
+    am = auto_mail(config_path)
+    am.send_email_msg(obj, msg)
+
+
+@logger.catch
+def job():
+    # 获取时间
+    ts = int(time.time() * 1000)
+    tme = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    logger.info(tme + "任务开始运行")
+    # 主程序
+    url = f"https://vip.stock.finance.sina.com.cn/forex/api/openapi.php/ForexService.getBankForex?callback=getAllBankForex&_={ts}"
+    ti = crawl_webpage(url)
+    res = parse_usd(ti, tme)
+    send_msg("外汇每日行情", res)
+
+
+if __name__ == "__main__":
+    logger.add("file_1.log", rotation="50 MB")    # Automatically rotate too big file
+    schedule.every().day.at("15:15").do(job)
+    job()
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
